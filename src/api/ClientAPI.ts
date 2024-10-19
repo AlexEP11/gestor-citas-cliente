cc83145c006199bc0609cc601c8012c979a7fd7a
AlexEP11 <aleeps2911@gmail.com>
2024-10-18 18:33:20 -0600

    Fix url in createCLient function

--- src/api/ClientAPI.ts
+++ src/api/ClientAPI.ts
@@ -3,7 +3,7 @@ import api from "../lib/axios";
 
 export async function createClient(formData: ClientFormData) {
     try {
-        const { data } = await api.post("/cliente", formData);
+        const { data } = await api.post("/cliente/", formData);
         console.log(data);
         return data;
     } catch (error) {
