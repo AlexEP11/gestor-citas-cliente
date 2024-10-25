8d6ad6ab29935ecb4ccf98275c9d593f22fbed36
AlexEP11 <aleeps2911@gmail.com>
2024-10-25 09:58:45 -0600

    Clean logs

--- src/components/schedule/Schedule.tsx
+++ src/components/schedule/Schedule.tsx
@@ -63,8 +63,6 @@ export default function Schedule() {
           }))
         : ([] as AppointmentFormDataSchedule[]);
 
-    console.log(events);
-
     const handleSelectEvent = (e: AppointmentFormDataSchedule) => {
         setSelectedCita(e);
         setModalOpen(true);
